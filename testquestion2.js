5badfba8a1be562b8f6c26ef4b34681de5cf84f3
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-04 10:13:19 +0000

    Sep 4, 2026, 10:13 AM

--- testquestion2.js
+++ testquestion2.js
@@ -3,15 +3,12 @@ let ypos = 50
 function setup() {
     createCanvas(600, 400);
     background("gray")
-    fill(0, 0, 0)
-    circle(xpos, ypos, 50)
-    xpos += 50
-    ypos += 50
+   
 }
 
 function draw() {
     for (let i = 0; i < 10 ; i++) { 
-        circle(xpos+i*50, ypos+i*50, 50)
+        circle(xpos+i*50, ypos+i*50, 50);
     }
 
 }
