a164441b0d5c62faec05ac803d596440176e7456
Computhink-CS143 <cs143@computhink.com.sg>
2026-09-04 10:15:39 +0000

    Sep 4, 2026, 10:15 AM

--- testquestion2.js
+++ testquestion2.js
@@ -10,7 +10,7 @@ function draw() {
     fill(colour, colour, colour)
     for (let i = 0; i < 10 ; i++) { 
         circle(xpos+i*50, ypos+i*50, 50);
-        colour
+        colour += 30
     }
 
 }
